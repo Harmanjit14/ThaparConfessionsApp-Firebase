a6a4db5046937b225ab5bf5981e00c874530ce86
Harmanjit Singh <harmanjit140500@gmail.com>
2020-09-11 09:24:02 +0530

    orientation fixed

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,16 @@ import 'package:TC/screen1.dart';
 import 'package:TC/screen2.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+ WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+    .then((_) {
+      runApp(new MyApp());
+    });
 }
 
 class MyApp extends StatelessWidget {
